a55f60c7f99544b88b10d0fe5100323ef63fe591
Thi Thanh Tuyen Nguyen <tnguyen7s@semo.edu>
2022-06-24 21:44:49 -0500

    finish navbar

--- src/app/shared/navigation-menu/navigation-menu.component.ts
+++ src/app/shared/navigation-menu/navigation-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
@@ -10,12 +12,18 @@ import { AuthService } from 'src/app/auth/auth.service';
 export class NavigationMenuComponent implements OnInit {
   public isExpanding = false;
   public image: string = null;
+  public nameCalled: string;
+  private sub: Subscription;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private accountService: AccountService) { }
 
   ngOnInit(): void {
     // load account image
     this.image = localStorage.getItem("image");
+    this.sub = this.accountService.userAccount.subscribe((value)=>{
+      const name = value.first_name+" " +value.last_name;
+      this.nameCalled = value.first_name || value.last_name? name:value.username;
+    })
   }
 
   onExpandRecordEmissionMenu(){
